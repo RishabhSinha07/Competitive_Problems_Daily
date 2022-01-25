72aa5a6677879f31d1605ab9fcd3ad36e590cd6e
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-01-26 00:19:24 +0530

    Time: 764 ms (35.96%), Space: 15.1 MB (90.05%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,5 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        '''
+        Move from destination to starting point. 
+        For each point calculate the min jumps required. 
+        Min jump can be found by getting the lowest just for the
+        next nums[i] elements from ith position and adding +1 to it
+        to execute the jump. 
+        
+        For index with value 0 assign the max threshold value as it is not
+        possible to jump from there and there is always a unique solution. 
+        Therefore it won't be an issue. 
+        
+        TC : O(N*K) where K is avg size of nums[i] and N is len of arr
+        SC : O(N) for dp
+        
+        '''
+        
         N = len(nums)
         dp = [0]*N
         
