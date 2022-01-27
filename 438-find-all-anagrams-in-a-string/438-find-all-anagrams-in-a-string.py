936cb1fa00eba2230ce911226532d0f671beed25
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-01-27 23:47:38 +0530

    Time: 92 ms (98.62%), Space: 15.3 MB (42.23%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -4,17 +4,20 @@ class Solution:
         r = collections.Counter(p)
         
         res = []
-        
+        temp = collections.defaultdict(int)
         for index, i in enumerate(s):
             if r[i] == 0:
-                while q: q.popleft()
+                while q: 
+                    val=q.popleft()
+                    temp[val]-=1
                 continue
                     
             q.append(i)
+            temp[i]+=1
             if len(q) == len(p):
-                temp = collections.Counter(q)
                 if temp == r:
                     res.append(index-len(p)+1)
-                q.popleft()
+                val = q.popleft()
+                temp[val]-=1
         
         return res
