c1aa0d60faa78d7ec2c6a9c17e04501279510364
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-01-27 22:01:49 +0530

    Time: 9796 ms (5.01%), Space: 14 MB (95.72%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,19 +1,20 @@
 class Solution:
-    def dfs(self, x, y, m, count, word):
+    
+    def dfs(self, board, i, j, count, word):
         if count == len(word):
             return True
-        if x<0 or x>=len(m) or y<0 or y>=len(m[0]) or m[x][y] != word[count]:
+        if i < 0 or i >= len(board) or j < 0 or j>= len(board[i]) or board[i][j] != word[count]:
             return False
-        temp = m[x][y]
-        m[x][y] = ''
-        if self.dfs(x+1,y,m,count+1,word) or self.dfs(x-1,y,m,count+1,word) or self.dfs(x,y+1,m,count+1,word) or self.dfs(x,y-1,m,count+1,word):
-            return True
-        m[x][y] = temp
-        return False
+        count+=1
+        temp = board[i][j]
+        board[i][j] = ' '
+        found = self.dfs(board,i+1,j,count,word) or self.dfs(board,i-1,j,count,word) or                                       self.dfs(board,i,j+1,count,word) or self.dfs(board,i,j-1,count,word)
+        board[i][j] = temp
+        return found
         
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0] and self.dfs(i,j,board,0,word):
+            for j in range(len(board[i])):
+                if board[i][j] == word[0] and self.dfs(board, i, j, 0, word):
                     return True
         return False
