ec4723ce0cb79be6376a024e4cac745513e121e1
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-01-28 11:58:45 +0530

    Time: 355 ms (54.21%), Space: 21.3 MB (99.96%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,50 +1,9 @@
-'''
-Trie is the way to go for sure. 
-
-The only thing different here is the "." mechanism which says dot matches all. 
-Therefore when we are searching in the trie and we encounter '.' we need to try
-all the values present in the child map of the node.
-
-TC : 500*N where N = len(input)
-'''
-class Node:
+class WordDictionary(object):
     def __init__(self):
-        self.child = {}
-        self.end = False
-
-class WordDictionary:
-
-    def __init__(self):
-        self.node = Node()
-
-    def addWord(self, word: str) -> None:
-        temp = self.node
-        for char in word:
-            if char not in temp.child:
-                temp.child[char] = Node()
-            temp = temp.child[char]
-        temp.end = True
-
-    def search(self, word: str) -> bool:
-        temp = self.node
-        
-        def dfs(node, pos, word):
-            if pos >= len(word):
-                return node.end
-            if word[pos] != '.' and word[pos] not in node.child:
-                return False
-            if word[pos] == '.':
-                for child_value in node.child:
-                    if dfs(node.child[child_value],pos+1, word):
-                        return True
-            else:
-                return dfs(node.child[word[pos]],pos+1, word)
-
-        
-        return dfs(temp, 0, word)
-
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+        self.dic = collections.defaultdict(list)
+    def addWord(self, word):
+        self.dic[len(word)].append(word)
+    def search(self, word):
+        for stored_word in self.dic[len(word)]:
+            if all(word[i] == stored_word[i] for i in range(len(word)) if word[i] != "."): return True
+        return False
