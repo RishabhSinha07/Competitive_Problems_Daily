7bf06f1ea1fb2cee97407be87fa611fcb9cb727e
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-01-30 00:46:23 +0530

    Time: 1339 ms (27.65%), Space: 69.7 MB (10.32%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,5 +1,9 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # Method 1 
+        # TC : O(n) 
+        # SC : O(n)
+        '''
         data = collections.defaultdict(int)
         for i in nums:
             data[i]+=1
@@ -7,3 +11,22 @@ class Solution:
         for i in range(1,2**32):
             if data[i]==0:
                 return i
+        '''
+        
+        # Method 2
+        '''
+        - If the maximun of the nums < 0 -> result = 1
+        - If all the values in range 1 to max is present -> result = max+1
+        - If value is missing return the value
+        TC : O(n)
+        SC : O(1)
+        '''
+        
+        max_value = max(nums)
+        if max_value <= 0:
+            return 1
+        temp = set(nums)
+        for i in range(1,max_value):
+            if i not in temp: return i
+        
+        return max_value+1
