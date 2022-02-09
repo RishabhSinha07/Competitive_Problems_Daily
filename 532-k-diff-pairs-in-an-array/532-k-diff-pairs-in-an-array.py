b0752117f21171bc1b7fb4c1b0dfad1f93de001e
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-09 14:41:14 +0530

    Time: 127 ms (36.81%), Space: 17 MB (7.00%) - LeetHub

---
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        '''
+        Brute force appx:
+        
+        For each element, iterate through the rest of the list
+        from i+1 to end give i is the current position. Check the cond
+        mod(nums[i]-nums[i+x]) == res and add to a set for unique ans.
+        
+        TC : O(n^2)
+        SC : O(1)
+        
+        For Faster Appx:
+        
+        - We can try to eliminate the searching part through the sub array. 
+        - We can leverage map for that. 
+        
+        - What we are trying to do in BF?
+        > For each current value we are trying to find if the following cond
+          satisfy:
+          - c-b = t : b = c-t 
+          - b-c = t : b = t+c
+          
+          we want to see if we have (t+c) or (c-t) in the map
+          
+          nums = [3,1,4,1,5], k = 2
+          - For 3 we want to check if (2+3 or 3-2) = (5,1) is present anywhere
+            in the list. 
+          ....... so on
+       
+       TC : O(N)
+       SC : O(N)
+     
+        '''
+        
+        map = collections.defaultdict(bool)
+        
+        result = set()
+        for i in nums[::-1]:
+            val1, val2 = k+i, i-k
+            if map[val1]:
+                temp = sorted([val1,i])
+                result.add(tuple(temp))
+            if map[val2]:
+                temp = sorted([val2,i])
+                result.add(tuple(temp))
+            
+            map[i] = True
+            
+        return len(result)
