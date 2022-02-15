d3cae2ed304ba9c7ce090774c208dee3ea53715c
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-15 11:30:14 +0530

    Time: 208 ms (43.46%), Space: 16.5 MB (96.68%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,9 +1,19 @@
 from collections import defaultdict
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        '''
+        # TC : O(n) SC : O(n)
         d = defaultdict(int)
         for val in nums:
             d[val]+=1
         for i in d.keys():
-            if d[i] == 1:
-                return i
+            if d[i] == 1:return i
+        '''
+        nums.sort()
+        for i in range(0,len(nums),2):
+            if i == len(nums)-1:
+                return nums[i]
+            if nums[i]!=nums[i+1]:
+                return nums[i]
+        
