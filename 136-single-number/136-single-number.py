3c1e602e92af58be51de112d070eeda805008bc2
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-15 11:34:28 +0530

    Time: 254 ms (23.90%), Space: 16.8 MB (30.94%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -10,6 +10,9 @@ class Solution:
         for i in d.keys():
             if d[i] == 1:return i
         '''
+        
+        '''
+        # TC : O(nlogn) SC : O(1)
         nums.sort()
         for i in range(0,len(nums),2):
             if i == len(nums)-1:
@@ -17,3 +20,9 @@ class Solution:
             if nums[i]!=nums[i+1]:
                 return nums[i]
         
+        '''
+        #TC : O(n) SC : O(1)
+        mask = 0
+        for i in nums:
+            mask^=i
+        return mask
