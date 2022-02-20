b26ac35b2ad0a2d4648f53d17447d23154eef0c2
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-20 14:56:14 +0530

    Time: 196 ms (14.81%), Space: 14.5 MB (86.23%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -1,5 +1,6 @@
 class Solution:
-    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+    def sol1(self,intervals):
+        # O(n^2) Time complexity
         res = []
         for i in range(len(intervals)):
             for j in range(len(intervals)):
@@ -11,4 +12,28 @@ class Solution:
         
         res = list(set(res))
         return len(intervals)-len(res)
-                    
+    
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x : x[0])
+        result = 0
+        cl, cr = intervals[0][0], intervals[0][1]
+        
+        for i in range(1,len(intervals)):
+            left, right = intervals[i][0], intervals[i][1]
+            
+            if left == cl and right == cr:
+                result+=1
+                continue
+            
+            if cl <= left and right <= cr:
+                result+=1
+                continue
+            
+            if left <= cl and cr <= right:
+                result+=1
+            
+            cl = left
+            cr = right
+                
+        
+        return len(intervals)-result
