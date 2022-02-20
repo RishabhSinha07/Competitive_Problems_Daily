685a3d6afaf533c4263d117dfaba7cc44bf295a9
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-20 14:40:49 +0530

    Time: 178 ms (22.08%), Space: 14.4 MB (96.36%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -3,12 +3,11 @@ class Solution:
         res = []
         for i in range(len(intervals)):
             for j in range(len(intervals)):
-                if i != j:
-                    a = intervals[i]
-                    b = intervals[j]
-                    if b[0]<=a[0] and a[1]<=b[1]:
-                        res.append((tuple(intervals[i])))
-                        break
+                a = intervals[i]
+                b = intervals[j]
+                if i != j and b[0]<=a[0] and a[1]<=b[1]:    
+                    res.append((tuple(intervals[i])))
+                    break
         
         res = list(set(res))
         return len(intervals)-len(res)
