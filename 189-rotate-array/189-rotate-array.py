0f592e4fe76e7fc2fcfc7bead71bfb5c92e93781
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-02-23 23:32:40 +0530

    Time: 4336 ms (5.00%), Space: 24.5 MB (99.55%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,26 +1,12 @@
-# class Solution:
-#     # O(n) TC and O(n) SC
-#     def rotate(self, nums: List[int], k: int) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         N, temp = len(nums), [x for x in nums]
-        
-#         for i in range(N):
-#             pos = (i+k)%N
-#             nums[pos] = temp[i]
-        
-
 class Solution:
-    # O(n) TC and O(1) SC
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         k = k%len(nums)
+        for _ in range(k):
+            temp = nums.pop()
+            nums.insert(0,temp)
         
-        nums.reverse()
-        nums[:k] = nums[:k][::-1]
-        nums[k:] = nums[k:][::-1]
         
         
