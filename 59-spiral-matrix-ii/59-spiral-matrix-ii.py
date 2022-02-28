592bf2a318f09ad4c76cf57a4fdad595dd5d97fa
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-03-01 01:42:03 +0530

    Time: 29 ms (91.07%), Space: 14.1 MB (45.75%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,19 +1,17 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         self.path = [[0,1],[1,0],[0,-1],[-1,0]]
-        self.visited = collections.defaultdict(bool)
         self.res = [[0]*n for _ in range(n)]
         self.pos = 0
         
         def insert(x,y,value):
-            if self.visited[(x,y)] or x >= n or y >= n or x < 0 or y < 0:
+            if x >= n or y >= n or x < 0 or y < 0 or self.res[x][y] != 0:
                 return
-            self.visited[(x,y)] = 1
             self.res[x][y] = value
             
             nx = x + self.path[self.pos][0]
             ny = y + self.path[self.pos][1]
-            if nx >= n or nx < 0 or ny >= n or ny < 0 or self.visited[(nx,ny)]:
+            if nx >= n or nx < 0 or ny >= n or ny < 0 or self.res[nx][ny] != 0:
                 self.pos+=1
                 self.pos%=4
             
