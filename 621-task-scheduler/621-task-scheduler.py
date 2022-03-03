19dd11a7ef096e4c35c3cfd3f34d5186c5d48c89
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-03-04 01:20:56 +0530

    Time: 935 ms (21.49%), Space: 14.5 MB (54.52%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -10,14 +10,13 @@ class Solution:
         processed = deque()
             
         while len(data) > 0 or len(processed) > 0:
-            if len(processed) > 0 and processed[0][1] <= current_time:
-                value = processed.popleft()
-                heappush(data,value[0])
             current_time+=1
             if len(data) > 0:
                 current = heappop(data)
                 current[0]+=1
                 if current[0] < 0:
                     processed.append([current,current_time+n])
-        
+            if len(processed) > 0 and processed[0][1] <= current_time:
+                value = processed.popleft()
+                heappush(data,value[0])
         return current_time
