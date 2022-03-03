2212a69b69bf129c2385f5de2062559ceefcece5
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-03-04 01:20:24 +0530

    Time: 880 ms (25.58%), Space: 14.6 MB (45.79%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        data, track = [], collections.Counter(tasks)
+        for i,v in track.items():
+            heappush(data,[-v,i])
+        
+        current_time = 0
+        processed = deque()
+            
+        while len(data) > 0 or len(processed) > 0:
+            if len(processed) > 0 and processed[0][1] <= current_time:
+                value = processed.popleft()
+                heappush(data,value[0])
+            current_time+=1
+            if len(data) > 0:
+                current = heappop(data)
+                current[0]+=1
+                if current[0] < 0:
+                    processed.append([current,current_time+n])
+        
+        return current_time
