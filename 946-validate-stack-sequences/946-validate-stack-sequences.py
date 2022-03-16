6f1eaf274c0fa5591bb0f11322e34f56b19dd1a4
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-03-16 12:45:14 +0530

    Time: 72 ms (92.53%), Space: 14.2 MB (63.80%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -1,12 +1,15 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        # Create variables to store pushed and popped information
         queue = collections.deque()
         popIndex = 0
         
         for i in pushed:
+            # If there is no value to pop then we cannot complete the process
             if popIndex >= len(popped):
                 return False
             queue.append(i)
+            # Try to pop as much value as possible
             while len(queue) > 0 and popIndex < len(popped):
                 if queue[-1] == popped[popIndex]:
                     queue.pop()
@@ -15,4 +18,11 @@ class Solution:
                     break
                     
         return popIndex >= len(popped) and len(queue) == 0
-            
+        
+        """
+        TC : O(N+M) where N = len(pushed) and M = len(popped)
+        SC : O(N)
+        
+        Runtime: 87 ms, faster than 69.83% of Python3 online submissions for Validate Stack Sequences.
+        Memory Usage: 14.1 MB, less than 93.97% of Python3 online submissions for Validate Stack Sequences.
+        """
