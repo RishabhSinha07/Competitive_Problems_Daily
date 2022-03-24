c7632b5bd9aef7cb500fa5ac99a0101ecbb9de35
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-03-24 12:42:47 +0530

    Time: 969 ms (6.57%), Space: 20.9 MB (80.59%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,20 +1,19 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        data = collections.defaultdict(int)
-        for i in people:data[i]+=1
+        data = sorted(people)
+        start, boats = 0, 0
+        end = len(data)-1
         
-        boats = 0
-        for i in people:
-            if data[i] <= 0:
+        for start in range(len(data)):
+            if data[start] == 'X':
                 continue
-            data[i]-=1
-            req_weight = limit-i
-            
-            while req_weight > 0:
-                if data[req_weight] > 0:
-                    data[req_weight]-=1
-                    break
-                req_weight-=1
+            req_weight = limit - data[start]
+            while end > 0 and (data[end] == 'X' or data[end] > req_weight):
+                end-=1
+            if data[end] != 'X' and data[end] <= req_weight:
+                data[end] = 'X'
             boats+=1
         
+        
         return boats
+        
