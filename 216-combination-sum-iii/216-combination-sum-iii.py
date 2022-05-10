5ccb9031d39438b27ee31dbea668d32b47878f01
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-05-10 21:52:14 +0530

    Time: 53 ms (29.27%), Space: 13.8 MB (79.39%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -9,6 +9,8 @@ Return a list of all possible valid combinations. The list must not contain the 
 k = 3 and N = 7
 -> 1,2,4
 
+TC : O(9**k)
+SC : O(1) considering we don't care about res
 """
 
 class Solution:
