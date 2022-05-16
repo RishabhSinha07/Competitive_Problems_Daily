00db3e4784d8b2e6cf5dd91d81ba25654bf5ab28
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-05-16 15:08:26 +0530

    Time: 604 ms (90.28%), Space: 14.3 MB (79.31%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,13 +2,13 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1:
             return -1
-        q = deque([[0,0,1,{}]])
+        q = deque([[0,0,1]])
         ex, ey = len(grid)-1, len(grid)-1 
         
         self.res = float('inf')
         
         while len(q) > 0:
-            cx, cy, dt, cache = q.popleft()
+            cx, cy, dt = q.popleft()
             
             if cx == ex and cy == ey:
                 self.res = min(self.res, dt)
@@ -17,9 +17,9 @@ class Solution:
             for i,j in [[0,1],[1,0],[1,1],[-1,0],[0,-1],[-1,-1],[-1,1],[1,-1]]:
                 nx = cx+i
                 ny = cy+j
-                if 0<=nx<=ex and 0<=ny<=ey and grid[nx][ny] == 0 and (nx,ny) not in cache:
-                    cache[(nx,ny)] = 1
-                    q.append([nx,ny,dt+1,cache])
+                if 0<=nx<=ex and 0<=ny<=ey and grid[nx][ny] == 0:
+                    grid[nx][ny] = 1
+                    q.append([nx,ny,dt+1])
                     
         
         if self.res == float('inf'):
