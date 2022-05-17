6ab47c384d31075788ba4897684809a87a3318c2
RishabhSinha07 <42510546+RishabhSinha07@users.noreply.github.com>
2022-05-17 14:37:21 +0530

    Time: 1948 ms (5.13%), Space: 25.1 MB (7.32%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -5,6 +5,8 @@
 #         self.left = None
 #         self.right = None
 
+"""
+# Without duplicates
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         
@@ -15,4 +17,33 @@ class Solution:
                 return node
             return dfs(node.left) or dfs(node.right)
         
+        return dfs(cloned)
+"""
+# With duplicates
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        
+        def deserialize(node, value):
+            if node is None:
+                value+='N'
+                return value
+            value+=str(node.val)
+            value+='('
+            value=deserialize(node.left,value)
+            value=deserialize(node.right,value)
+            value+=')'
+            
+            return value
+        
+        new_target = deserialize(target, "")
+        
+        def dfs(node):
+            if node is None:
+                return None
+            nonlocal new_target
+        
+            if deserialize(node,"") == new_target:
+                return node
+            return dfs(node.left) or dfs(node.right)
+            
         return dfs(cloned)
